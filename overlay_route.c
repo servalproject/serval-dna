a4591a0d870e00e9a977aab51f5b4ffc229b9f5c
gardners <paul@servalproject.org>
2011-08-09 18:51:50 +0200

    I forgot.

--- overlay_route.c
+++ overlay_route.c
@@ -2,6 +2,25 @@
 
 int overlay_add_selfannouncement(overlay_buffer *b)
 {
+  /* Pull the first record from the HLR database and turn it into a
+     self-announcment. These are shorter than regular Subscriber Observation
+     Notices (SON) because they are just single-hop announcments of presence.
+
+     Do we really need to push the whole SID (32 bytes), or will just, say, 
+     8 do so that we use a prefix of the SID which is still very hard to forge.
+     
+     XXX A hearer of a self-announcement who has not previously seen the sender might
+     like to get some authentication to prevent naughty people from spoofing routes.
+     This should be easy enough to do, but will need some thought.
+  */
+
+  
+
+  if (ob_append_bytes(b,OF_SELFANNOUNCE,sizeof(OF_SELFANNOUNCE)))
+    return WHY("ob_append_bytes() could not add self-announcement header");
+  
+  
+
   return WHY("Not implemented");
 }
 
