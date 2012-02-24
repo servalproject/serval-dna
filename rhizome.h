a8411edf3f6b416dfaa5ac56f5459a69e9f034b1
Daniel O'Connor <doconnor@gsoft.com.au>
2012-02-24 17:21:22 +1030

    - Use system wide sqlite3 lib via pkg-config - Remove nacl-gcc-prep, it's now in nacl - Sort sources and have 1 per line. - Add missing source files. - Use naclinc.txt and nacllib.txt generated by nacl/nacl-gcc-prep - Update ignore file now nacl files don't pollute this directory.

--- rhizome.h
+++ rhizome.h
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include "sqlite-amalgamation-3070900/sqlite3.h"
+#include <sqlite3.h>
 #include "sha2.h"
 #include <sys/stat.h>
 
