03275f410f5b4fac7f306a9dca919f6e9042824a
Daniel O'Connor <daniel@servalproject.org>
2012-09-06 15:26:40 +0930

    Include stdint.h for intmax_t.

--- log.c
+++ log.c
@@ -31,6 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdint.h>
 
 #include "log.h"
 #include "net.h"
