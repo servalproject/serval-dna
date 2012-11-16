f5a6dc7799878f3f002ca57ce26f68e95d71c6f1
Romain Vimont (®om) <rom@rom1v.com>
2012-11-16 15:31:28 +0100

    overlay_mdp_bind must be visible in MDP API

--- mdp_client.h
+++ mdp_client.h
@@ -25,6 +25,7 @@
 int overlay_mdp_client_socket(void);
 int overlay_mdp_client_close(int mdp_sockfd);
 int overlay_mdp_client_poll(int mdp_sockfd, time_ms_t timeout_ms);
+int overlay_mdp_bind(int mdp_sockfd, unsigned char *localaddr, int port);
 int overlay_mdp_recv(int mdp_sockfd, overlay_mdp_frame *mdp, int port, int *ttl);
 int overlay_mdp_send(int mdp_sockfd, overlay_mdp_frame *mdp,int flags,int timeout_ms);
 int overlay_mdp_relevant_bytes(overlay_mdp_frame *mdp);
