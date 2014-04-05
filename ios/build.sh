540401c472893372de1555197d2f77324f1521e7
James Moore <hello@jmoore.me>
2014-04-05 12:17:33 -0700

    adding Xcode wrapper project

--- ios/build.sh
+++ ios/build.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 #  build.sh
-#  serval-dna
+#  servald
 #
 #  Created by James Moore on 3/25/14.
 #  Copyright (c) 2014 The Serval Project. All rights reserved.
 
-set -x
+# set -x
+
+# if we're building inside of xcode we need to back up a level
+if [[ -n $DEVELOPER_DIR ]]; then
+	cd ..
+	pwd
+fi
 
 # Add the homebrew tools to the path since automake no longer is apart of Xcode
 PATH=/usr/local/bin:$PATH
@@ -39,18 +45,23 @@ buildIOS()
 	export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot $SDKROOT -I$SDKROOT/usr/include -miphoneos-version-min=${SDK_VERSION}"
 	export CC="clang"
 	
-	echo "Building serval-dna for ${PLATFORM} ${SDK_VERSION} ${ARCH}"
+	echo "=> Building servald for ${PLATFORM} ${SDK_VERSION} ${ARCH}"
 
-	./configure $HOST --prefix="${PREFIX}/serval-dna-${ARCH}" --disable-voiptest &> "${PREFIX}/serval-dna-${ARCH}.log"
+	./configure $HOST --prefix="${PREFIX}/servald-${ARCH}" --disable-voiptest &> "${PREFIX}/servald-${ARCH}.log"
 
-	make >> "${PREFIX}/serval-dna-${ARCH}.log" 2>&1
-	make install >> "${PREFIX}/serval-dna-${ARCH}.log" 2>&1
-	make clean >> "${PREFIX}/serval-dna-${ARCH}.log" 2>&1
+	make >> "${PREFIX}/servald-${ARCH}.log" 2>&1
+	make install >> "${PREFIX}/servald-${ARCH}.log" 2>&1
+	make clean >> "${PREFIX}/servald-${ARCH}.log" 2>&1
 	
 	# don't know why these don't get removed
 	rm directory_service.o
 	rm config_test.o
 }
+
+#
+# Start the build
+#
+
 
 # remove duplicated function
 perl -p -i -e 's/^(void rotbuf_log\(struct __sourceloc __whence, int log_level, const char \*prefix, const struct rotbuf \*rb\);)/\/\/\1/' rotbuf.h
@@ -59,26 +70,25 @@ perl -p -i -e 's/^(void rotbuf_log\(struct __sourceloc __whence, int log_level, 
 perl -p -i -e 's/^\t\$\(INSTALL_PROGRAM\) -D servald \$\(DESTDIR\)\$\(sbindir\)\/servald/\tmkdir -p \$\(DESTDIR\)\$\(sbindir\)
 \t\$\(INSTALL_PROGRAM\) servald \$\(DESTDIR\)\$\(sbindir\)\/servald/' Makefile.in
 
-# use CFLAGS when building version file to support cross-compilation
-# perl -p -i -e 's/&& \$\(CC\) -c version_servald.c/&& \$\(CC\) \$\(CFLAGS\) -c version_servald.c/' Makefile.in
-
 # Generate configure
 autoreconf -f -i
 
-#mkdir -p build/include/serval-dna
-rm -rf "${PREFIX}/serval-dna-*"
+rm -rf ${PREFIX}/servald-*
 
 for arch in ${ARCHS}; do
 	buildIOS "${arch}"
 done
 
-echo "Building fat binary"
+echo "=> Building fat binary"
 
 lipo \
-	"${PREFIX}/serval-dna-armv7/sbin/servald" \
-	"${PREFIX}/serval-dna-armv7s/sbin/servald" \
-	"${PREFIX}/serval-dna-arm64/sbin/servald" \
-	"${PREFIX}/serval-dna-i386/sbin/servald" \
-	"${PREFIX}/serval-dna-x86_64/sbin/servald" \
+	"${PREFIX}/servald-armv7/sbin/servald" \
+	"${PREFIX}/servald-armv7s/sbin/servald" \
+	"${PREFIX}/servald-arm64/sbin/servald" \
+	"${PREFIX}/servald-i386/sbin/servald" \
+	"${PREFIX}/servald-x86_64/sbin/servald" \
 	-create -output ${PREFIX}/servald
 
+rm -rf ${PREFIX}/servald-*
+
+echo "=> Done"
