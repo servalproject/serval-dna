1f49ee47ae442de13feb2c0369cc1f74797ab510
Andrew Bettison <andrew@servalproject.org>
2014-04-07 14:39:21 +0930

    Add missing copyright messages

--- mdp_filter.c
+++ mdp_filter.c
@@ -1,3 +1,21 @@
+/*
+ MDP packet filtering
+ Copyright (C) 2013-2014 Serval Project Inc.
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 #include "serval.h"
 #include "overlay_address.h"
@@ -99,4 +117,3 @@ void load_mdp_packet_rules(const char *UNUSED(filename))
    * 
    * */
 }
-
--- msp_client.h
+++ msp_client.h
@@ -1,5 +1,24 @@
-#ifndef __SERVALD_MSP_CLIENT_H
-#define __SERVALD_MSP_CLIENT_H
+/*
+ Mesh Streaming Protocol (MSP)
+ Copyright (C) 2013-2014 Serval Project Inc.
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __SERVAL_DNA__MSP_CLIENT_H
+#define __SERVAL_DNA__MSP_CLIENT_H
 
 #define MSP_STATE_UNINITIALISED 0
 #define MSP_STATE_LISTENING (1<<0)
@@ -54,4 +73,4 @@ int msp_recv(int mdp_sock);
 // next_action indicates the next time that msp_processing should be called
 int msp_processing(time_ms_t *next_action);
 
-#endif
+#endif //__SERVAL_DNA__MSP_CLIENT_H
