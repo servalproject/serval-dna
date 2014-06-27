537a09812c1aeebaaa6518bdbf56a619a63e5061
Jeremy Lakeman <Jeremy.Lakeman@gmail.com>
2014-06-27 12:45:53 +0930

    Ignore some types of sendto errors

--- overlay_interface.c
+++ overlay_interface.c
@@ -945,12 +945,13 @@ int overlay_broadcast_ensemble(struct network_destination *destination, struct o
 		  bytes, (size_t)len, 0, 
 		  &destination->address.addr, destination->address.addrlen);
 	if (sent == -1){
-	  WHYF_perror("sendto(fd=%d,len=%zu,addr=%s) on interface %s",
-	      interface->alarm.poll.fd,
-	      (size_t)len,
-	      alloca_socket_address(&destination->address),
-	      interface->name
-	    );
+	  if (errno!=EAGAIN && errno!=EWOULDBLOCK && errno!=ENOENT && errno!=ENOTDIR)
+	    WHYF_perror("sendto(fd=%d,len=%zu,addr=%s) on interface %s",
+		interface->alarm.poll.fd,
+		(size_t)len,
+		alloca_socket_address(&destination->address),
+		interface->name
+	      );
 	  // close the interface if we had any error while sending broadcast packets,
 	  // unicast packets should not bring the interface down
 	  // TODO mark unicast destination as failed?
